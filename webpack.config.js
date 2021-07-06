42cd3a2afaaa137e1c71644e9d89ae62be778913
Max <max.lin@screeningeagle.com>
2021-07-06 11:15:54 +0800

    Update file-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -53,11 +53,11 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                  },
-                ],
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images'
+                },
               }
         ]
     },
