07b035044f443f896bdae734804ff82e65cc391b
Max <max.lin@screeningeagle.com>
2021-07-06 11:01:31 +0800

    Enhance msg

--- app/formatter/formatter.js
+++ app/formatter/formatter.js
@@ -1,9 +1,41 @@
 import React, { Fragment } from "react";
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl';
+import { Typography } from 'antd';
+
+const { Text } = Typography
 
 const FormatRoundResults = (roundResult) => {
     const castAbilityFormatID = `CastAbility.${roundResult.castAbility.type}`
     const roundResultFormatID = `RoundResult.${roundResult.castAbility.effect}`
+
+    let AbilityName
+    let AbilityValue
+    switch (roundResult.castAbility.type) {
+        case 'General':
+            AbilityName = (<Text strong>{roundResult.castAbility.name}</Text>)
+            AbilityValue = (<Text strong>{roundResult.castAbility.value}</Text>)
+            break;
+        case 'Skill':
+            AbilityName = (<Text type="warning" strong>
+                <div style={{fontSize: '1.25rem'}}>
+                    {roundResult.castAbility.name}
+                </div>
+            </Text>)
+            AbilityValue = (<Text type="warning">{roundResult.castAbility.value}</Text>)
+            break;
+        case 'Ultimate':
+            AbilityName = (<Text type="danger" strong italic>
+                <div style={{fontSize: '1.5rem'}}>
+                    {roundResult.castAbility.name} ！！！
+                </div>
+            </Text>)
+            AbilityValue = (<Text type="danger">{roundResult.castAbility.value}</Text>)
+            break;
+        default:
+            AbilityName = roundResult.castAbility.name
+            AbilityValue = roundResult.castAbility.value
+            break;
+    }
 
     return (
         <Fragment>
@@ -11,16 +43,13 @@ const FormatRoundResults = (roundResult) => {
                 id={castAbilityFormatID}
                 values={{
                     attacker: roundResult.attacker,
-                    abilityName: roundResult.castAbility.name,
+                    abilityName: AbilityName
                 }}
             />
             <br />
             <FormattedMessage
                 id={roundResultFormatID}
-                values={{
-                    receiver: roundResult.defender,
-                    value: roundResult.castAbility.value,
-                }}
+                values={{ value: AbilityValue }}
             />
         </Fragment>
     )
--- app/i18n/zh.js
+++ app/i18n/zh.js
@@ -3,8 +3,8 @@ export default {
     messages: {
         'CastAbility.General': '{attacker} 使出 {abilityName}',
         'CastAbility.Skill': '{attacker} 施放技能 {abilityName}',
-        'CastAbility.Ultimate': '{attacker} 發動大決： {abilityName} ！！！！',
-        'RoundResult.Damage': '{receiver} 受到了 {value} 點傷害',
+        'CastAbility.Ultimate': '{attacker} 發動大決 {abilityName}',
+        'RoundResult.Damage': '造成 {value} 點傷害',
         'RoundResult.Heal': '{receiver} 恢復了 {value} 點生命',
         'Combat.Result': '恭喜 {winner} 獲得勝利',
         'Combat.LowHP': '{fighter} 快被打掛啦！！！！',
