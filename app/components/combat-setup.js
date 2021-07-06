b913d7c5e1ea8fbb584d5ef02bbe2a9fc066f4d9
Max <max.lin@screeningeagle.com>
2021-07-06 19:33:57 +0800

    Add form validation

--- app/components/combat-setup.js
+++ app/components/combat-setup.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, message } from 'antd'
 import { v4 as uuidv4 } from 'uuid';
 
 const CombatSetup = ({ isFighting, combatSetupHandler }) => {
     const [fighters, setFighters] = useState([])
 
     const setupHandler = ({ fighterA, fighterB }) => {
-        const newFighters = [fighterA, fighterB]
-        const newRoundID = uuidv4()
-        setFighters(newFighters)
-        combatSetupHandler(newFighters, newRoundID)
+        if (fighterA && fighterB && fighterA !== fighterB) {
+            const newFighters = [fighterA, fighterB]
+            const newRoundID = uuidv4()
+            setFighters(newFighters)
+            combatSetupHandler(newFighters, newRoundID)
+        } else if (!fighterA && !fighterB) {
+            message.warning("沒有猶豫的選擇嗎？")
+        } else if (!fighterA || !fighterB) {
+            message.warning("你只填了一個選擇...")
+        } else {
+            message.warning("既然只有一個選擇又何必猶豫？")
+        }
     }
     const fields = [{
         name: ['fighterA'],
@@ -21,10 +29,19 @@ const CombatSetup = ({ isFighting, combatSetupHandler }) => {
 
     return (
         <Form fields={fields} onFinish={setupHandler}>
-            <Form.Item label="選擇 1" name="fighterA">
-                <Input placeholder='Ex: 麥當勞' disabled={isFighting} />
+            <Form.Item
+                label="選擇 1"
+                name="fighterA"
+            >
+                <Input
+                    placeholder='Ex: 麥當勞'
+                    disabled={isFighting}
+                />
             </Form.Item>
-            <Form.Item label="選擇 2" name="fighterB">
+            <Form.Item
+                label="選擇 2"
+                name="fighterB"
+            >
                 <Input placeholder='Ex: 肯德基' disabled={isFighting} />
             </Form.Item>
             <Form.Item>
