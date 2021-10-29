3af5940fe5c1b75fe11fd0820057befc80a1fe47
Max <max.lin@screeningeagle.com>
2021-10-29 11:06:23 +0800

    Apply i18n by browser lang

--- app/index.js
+++ app/index.js
@@ -4,12 +4,21 @@ import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl'
 import App from './components/app'
 import zh from './i18n/zh'
+import en from './i18n/en'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
+const i18n = () => {
+    const lang = navigator.language || navigator.userLanguage
+    if (lang.split("-")[0] === 'zh') {
+        return zh
+    }
+    return en
+}
+
 ReactDOM.render(
     <Provider store={store}>
-        <IntlProvider messages={zh.messages} locale={zh.lang} defaultLocale="zh">
+        <IntlProvider messages={i18n().messages} locale={i18n().lang}>
             <App />
         </IntlProvider>
     </Provider>,
